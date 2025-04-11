0d594abefe5b27b99972014e0ee7a4d0d0339980
Lakshay Gupta <lakshaygupta2511@gmail.com>
2025-04-11 17:31:43 +0530

    Fixed the backend link

--- frontend/src/constant.tsx
+++ frontend/src/constant.tsx
@@ -1,2 +1,2 @@
-export const LINK = "https://community-frontend-f890.onrender.com/";
+export const LINK = "https://community-frontend-f890.onrender.com";
 // export const LINK = "http://localhost:4000";
